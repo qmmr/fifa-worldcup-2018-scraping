aae72288412cb3ed08321fa6095b9ef209db8ad4
Marcin Kumorek <kumeek@gmail.com>
2018-06-23 18:20:39 +0200

    Refactor to use teams from DB

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const findMatches = matches => {
   })
 }
 
-const asyncRequest = (url, options, ...args) =>
+const asyncRequest = (url, options) =>
   new Promise((resolve, reject) => {
     request(url, (err, response, html) => {
       if (!err && response.statusCode === 200) {
@@ -34,78 +34,88 @@ const asyncRequest = (url, options, ...args) =>
     })
   })
 
-const createMatchData = $match => {
+const createMatchData = ($match, teams) => {
   const matchURI = $match.attr('href')
   const $status = $match.find('.fi-mu__status .fi-s__status .post_match')
   const status = $status.text().trim()
   const finished = !$status.hasClass('hidden')
-  const $teamHome = $match.find('.fi-mu__m .home')
-  const teamHomeId = $teamHome.data('team-id')
-  const $teamAway = $match.find('.fi-mu__m .away')
-  const awayTeamId = $teamAway.data('team-id')
+  const $homeTeam = $match.find('.fi-mu__m .home')
+  const $awayTeam = $match.find('.fi-mu__m .away')
+  const homeTeamISO2 = $homeTeam.find('.fi-t__nTri').text()
+  const awayTeamISO2 = $awayTeam.find('.fi-t__nTri').text()
+  const teamHomeId = $homeTeam.data('team-id')
+  const awayTeamId = $awayTeam.data('team-id')
+  // {
+  // 	name: $homeTeam.find('.fi-t__nText').text(),
+  // 	iso2: $homeTeam.find('.fi-t__nTri').text(),
+  // 	image: $homeTeam.find('img').attr('src'),
+  // }
+  // {
+  // 	name: $awayTeam.find('.fi-t__nText').text(),
+  // 	iso2: $awayTeam.find('.fi-t__nTri').text(),
+  // 	image: $awayTeam.find('img').attr('src'),
+  // }
+
+  // Find teams from DB teams
+  const homeTeam = teams.filter(team => team.fifaCode === homeTeamISO2)[0]
+  const awayTeam = teams.filter(team => team.fifaCode === awayTeamISO2)[0]
   const matchData = {
     status,
     matchURI,
     finished,
+    _typeName: 'Game', // GraphQL type
     datetime: $match.find('.fi-mu__info__datetime').data('utcdate'),
     matchURI: $match.attr('href'),
     stadium: $match.find('.fi__info__stadium').text(),
     venue: $match.find('.fi__info__venue').text(),
-    homeTeam: {
-      name: $teamHome.find('.fi-t__nText').text(),
-      iso2: $teamHome.find('.fi-t__nTri').text(),
-      image: $teamHome.find('img').attr('src'),
-    },
+    homeTeam,
     score: $match
       .find('.fi-s__score .fi-s__scoreText')
       .text()
       .trim(),
-    awayTeam: {
-      name: $teamAway.find('.fi-t__nText').text(),
-      iso2: $teamAway.find('.fi-t__nTri').text(),
-      image: $teamAway.find('img').attr('src'),
-    },
+    awayTeam,
   }
 
   return matchData
 }
 
-MongoClient.connect(
-  `mongodb://${HOST}:${PORT}/${DB}`,
-  async (err, database) => {
-    if (err) throw err
+MongoClient.connect(`mongodb://${HOST}:${PORT}/${DB}`).then(database => {
+  const db = database.db(DB)
+  console.log(`You're connected to: mongodb://${HOST}:${PORT}/${DB}\n`)
+  const matches = []
+  let teams = []
 
-    const db = database.db(DB)
-    console.log(`You're connected to: mongodb://${HOST}:${PORT}/${DB}\n`)
-    const matches = []
+  // fetch teams from DB
+  db.collection('teams')
+    .find({})
+    .toArray(async (err, docs) => {
+      teams = [...docs]
+      console.log(`Found ${teams.length} teams\n`)
 
-    try {
-      const { response, html } = await asyncRequest(URL)
-      const $ = cheerio.load(html)
-      $('.fi-mu__link').each((idx, match) => {
-        const matchData = createMatchData($(match))
-        if (matchData.finished) {
-          console.log(`${++idx}. ${matchData.homeTeam.name} ${matchData.score} ${matchData.awayTeam.name}`)
-          matches.push(matchData)
-        }
-      })
-
-      // After parsing all games from html we can insert them to the DB
-      if (matches.length) {
-        console.log('Found new games, inserting into DB...\n')
-        db.collection(COLLECTION).insertMany(matches, (err, doc) => {
-          if (err) throw err
-          console.log('Successfully inserted: ', JSON.stringify(doc))
+      try {
+        const { response, html } = await asyncRequest(URL)
+        const $ = cheerio.load(html)
+        $('.fi-mu__link').each((idx, match) => {
+          const matchData = createMatchData($(match), teams)
+          if (matchData.finished) {
+            console.log(`${++idx}. ${matchData.homeTeam.name} ${matchData.score} ${matchData.awayTeam.name}`)
+            matches.push(matchData)
+          }
         })
-      }
-    } catch (error) {
-      console.error(error)
-    } finally {
-      console.info('\nClosing connection to the DB\nBye!\n')
-      database.close()
-    }
 
-    //   console.log('Number of matches: ', matches.length)
-    //   const newMatches = await findMatches(matches)
-  }
-)
+        // After parsing all games from html we can insert them to the DB
+        if (matches.length) {
+          console.log(`Found ${matches.length} matches, inserting into DB...\n`)
+          db.collection(COLLECTION).insertMany(matches, (err, doc) => {
+            if (err) throw err
+            console.log(`Successfully inserted ${doc.insertedCount} documents 👍`)
+          })
+        }
+      } catch (error) {
+        console.error(error)
+      } finally {
+        console.info('\nClosing connection to the DB\nBye!\n')
+        database.close()
+      }
+    })
+})
